a1f9400535c95e2a15a2cc0cc895f53d9cb24c88
Eduardo1749 <dudu.evangelista1749@gmail.com>
2022-02-22 08:50:00 -0300

    Interceptor para requisiçoes http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -75,8 +76,7 @@ import { ToastrModule } from 'ngx-toastr';
     }),
     //   NgxMaskModule.forRoot()
   ],
-  // AuthInterceptorProvider -- é oq vai no [] de baixo
-  providers: [],
+  providers: [AuthInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
