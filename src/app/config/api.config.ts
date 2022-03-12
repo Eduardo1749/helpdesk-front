e41ac72603f90799e9bb3b431082e56b3ace77c4
Eduardo1749 <dudu.evangelista1749@gmail.com>
2022-03-12 11:44:02 -0300

    Componente TECNICO-DELETE criado

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : 'https://helpdesk-project.herokuapp.com'
+    baseUrl : 'https://helpdesk-project.herokuapp.com' 
+    // baseUrl : 'http://localhost:8080' //Para testar com o projeto que está na minha máquina, ou seja, testar localmente
 }
