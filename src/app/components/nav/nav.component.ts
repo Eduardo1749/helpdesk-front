a8da5b85dc9048c456282e5cd76e63a03cf9cb2f
Eduardo1749 <dudu.evangelista1749@gmail.com>
2022-03-17 18:34:12 -0300

    Rota para componente CHAMADO-UPDATE

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -16,7 +16,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['chamados/update/1'])//Lembrar de trocar para login, pois é o componente desse path que será o primeiro a ser renderizado na tela.
+    this.router.navigate(['chamados/read/1'])//Lembrar de trocar para login, pois é o componente desse path que será o primeiro a ser renderizado na tela.
   }
 
   logout(){
